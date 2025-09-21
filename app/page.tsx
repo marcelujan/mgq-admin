9e4796ff86ca2af0ff29502abc41e6475d5a98d0
Marcelo Luján <mlujan1863@gmail.com>
2025-09-21 14:43:52 -0300

    deploy: auto

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import React from "react";
-import HomeTabs, { useHashTab } from "./components/HomeTabs";
-import ProveedorView from "./components/ProveedorView";
-import VentasView from "./components/VentasView";
-import FormuladosView from "./components/FormuladosView";
-
-export default function HomePage() {
-  const { tab, change } = useHashTab("proveedor");
-
-  return (
-    <div className="max-w-7xl mx-auto p-4">
-      <HomeTabs active={tab} onChange={change} />
-      {tab === "proveedor" && <ProveedorView />}
-      {tab === "ventas" && <VentasView />}
-      {tab === "formulados" && <FormuladosView />}
-    </div>
-  );
+export default function Home() {
+  redirect("/proveedor");
 }
