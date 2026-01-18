6893a36a7d0c9e1a422f1f3d6bed312fa860ab01
marcelujan <marcelo.lujan@mi.unc.edu.ar>
2026-01-18 14:20:06 -0300

    deploy: auto

--- 00p_convertir-v2-en-main.bat
+++ 00p_convertir-v2-en-main.bat
@@ -9,7 +9,7 @@ git status --porcelain | findstr . >nul && (
 git checkout main || exit /b 1
 git pull --rebase origin main || exit /b 1
 
-git merge prueba-preview
+git merge v2
 if errorlevel 1 (
   echo Hubo conflictos. Resolve en VSCode, luego:
   echo   git add -A
